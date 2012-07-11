4c5cc9c9d3fa2c1ece66746bbaa4ea3c44b87579
Maurizio De Magnis <maurizio.demagnis@gmail.com>
2012-07-11 18:34:51 +0200

    Genesis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrapgallery::Application.config.secret_token = 'c3e556f77ec16520e4029fbd0d6891a6053409f0aaaf4d8ce22b55b09244870d74d564bc03b13d7d9adca1a1a59af714d3cc961583726a90a174e096afe3c343'
