d6abf8c418f4357dc79e6b07960d3eae65181744
Maurizio De Magnis <maurizio.demagnis@gmail.com>
2012-07-13 10:38:47 +0200

    Removes Postgres and adds Mongo

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# require 'rails/all'
+
+# require 'active_record'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'active_resource/railtie'
+require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -13,6 +20,8 @@ module Scrapgallery
   class Application < Rails::Application
     # Heroku requires this to make deployments work.
 config.assets.initialize_on_precompile = false
+    Mongoid.logger.level = Logger::DEBUG
+    Moped.logger.level = Logger::DEBUG
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -54,7 +63,7 @@ config.assets.initialize_on_precompile = false
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
